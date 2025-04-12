a20bda666d0acaf838dd7cc4b88b60f6fdca2df8
CamiSerantoni <camila.serantonir@gmail.com>
2025-04-12 09:03:06 -0400

    handling not found situatiuons

---
+++ app/news/[slug]/not-found.js
@@ -0,0 +1,6 @@
+export default function NewsNotFoundPage(){
+    return <div id="error">
+        <h1> Not found!</h1>
+        <p> Unfortunely, we could not find the requested article.</p>
+    </div>
+}
--- app/news/[slug]/page.js
+++ app/news/[slug]/page.js
@@ -1,17 +1,25 @@
 import { DUMMY_NEWS } from "@/dummy-news";
+import { notFound } from "next/navigation";
 
 export default function NewsDetailPage({ params }) {
-    const newsSlug= params.slug;
-    const newsItem = DUMMY_NEWS.find(newsItem => newsItem.slug === newsSlug )
-    return (
-        <article className="news-article">
-            <header>
-                    <h1>{newsItem.title}</h1>
-                    <img src={`/images/news/${newsItem.image}`} alt={newsItem.title}/>
-                    <time dateTime={newsItem.date}>{newsItem.date}</time>
-            </header>
-        
-            <p>{newsItem.content}</p>
-        </article>
-    );
+  const newsSlug = params.slug;
+  const newsItem = DUMMY_NEWS.find((newsItem) => newsItem.slug === newsSlug);
+
+
+
+  if (!newsItem) {
+    notFound()
+    }
+
+  return (
+    <article className="news-article">
+      <header>
+        <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+        <h1>{newsItem.title}</h1>
+        <time dateTime={newsItem.date}>{newsItem.date}</time>
+      </header>
+
+      <p>{newsItem.content}</p>
+    </article>
+  );
 }
