bb1da350a6c4f8152fd405fba3f6110dc53eb07c
CamiSerantoni <camila.serantonir@gmail.com>
2025-04-28 23:52:29 -0400

    ading default page

--- app/archive/@archive/[year]/page.js
+++ app/archive/@archive/[year]/page.js
@@ -1,7 +1,8 @@
 import NewsList from "@/components/news-list";
+import { getNewsForYear } from "@/lib/news";
 
 export default function FilteredNewsPage({params}){
-    const newsYear =params.newsYear;
+    const newsYear =params.year;
     const news =  getNewsForYear(newsYear)
 
     return <NewsList news={news}/>
